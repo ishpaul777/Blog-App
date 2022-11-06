ff30c1cf4a995bf6ed965b8ff21549da9dd45546
Ishpaul Singh <ishpaul207@gmail.com>
2022-11-06 19:40:12 +0530

    added controller and view for posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,12 @@
+class PostsController < ApplicationController
+  def index
+    # find all posts of this user
+    @posts = Post.where(author_id: params[:user_id])
+    @user = User.find(params[:user_id])
+  end
+
+  def show
+    # find all posts of this user
+    @post = Post.find(params[:id])
+  end
+end
