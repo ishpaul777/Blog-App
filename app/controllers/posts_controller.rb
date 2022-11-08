09d398688b7e002215dd8716bbb4b191e5bb6064
Ishpaul Singh <ishpaul207@gmail.com>
2022-11-08 14:23:46 +0530

    added tests

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,11 @@ class PostsController < ApplicationController
   def index
     # find all posts of this user
     @posts = Post.where(author_id: params[:user_id])
-    @user = User.find(params[:user_id])
+    @user = User.where(id: params[:user_id])
   end
 
   def show
     # find all posts of this user
-    @post = Post.find(params[:id])
+    @post = Post.where(id: params[:id])
   end
 end
